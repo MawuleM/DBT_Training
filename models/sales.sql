e1738347dd1d4867e7ae8ed8dba134bd6bdaf0c5
mawule08 <miassimarys08@gmail.com>
2025-10-22 02:02:26 +0200

    Mise à jour 1

--- models/sales.sql
+++ models/sales.sql
@@ -1,3 +1,8 @@
-select 
-    *
-from `aivancity-learn.sources_tables.fact_sales`
+CREATE TABLE `aivancity-learn.sources_tables.montly_new_customers` AS
+SELECT DATE_TRUNC (signup_date, MONTH) as month,
+        count(customer_id) as new_customers
+    FROM `aivancity-learn.sources_tables.dim_customers`
+GROUP BY month;
+
+SELECT *
+FROM `aivancity-learn.sources_tables.montly_new_customers`
